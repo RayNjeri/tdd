f65b4ffc1691929e329149c63db51c21c6db02e4
Rachael Njeri <njerirachael0@gmail.com>
2017-03-06 18:25:24 +0300

    modulo test

--- test_tdd.py
+++ test_tdd.py
@@ -13,6 +13,11 @@ class Test_solution(unittest.TestCase):
 	def test_multiplication(self):
 		self.assertTrue(test_solution.solution(10,20,"*"),200)
 
+	def test_division(self):
+		self.assertTrue(test_solution.solution(20,5,"/"),4)
+
+	def test_modulo(self):
+		self.assertTrue(test_solution.solution(20,3,"%"),2)	
 
 if __name__=="__main__":
     unittest.main()
